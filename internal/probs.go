069d11e154b513ee4843b3a22bd0b17e85558916
Matthew Sunner <sunner.matt@gmail.com>
2025-06-13 21:59:55 -0500

    Remove whitespace in docstring for func

--- internal/probs.go
+++ internal/probs.go
@@ -21,7 +21,6 @@ func Sum(intSlice []int) int {
 // Find the sum of all the multiples of 3 or 5 below 1000.
 //
 // https://projecteuler.net/problem=1
-
 func ProbOne(countOf int, maxOf int) int {
 	solutionsList := make([]int, 0, countOf)
 
